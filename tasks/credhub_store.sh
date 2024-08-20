46451dc52b4b96757301120661200d29f349fe45
itgrl-bex <rerobinson@vmware.com>
2024-08-20 11:37:10 -0400

    Added task to accept a template, interpolate, and store to credhub.

---
+++ tasks/credhub_store.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+# code_snippet credhub-get-script start bash
+
+cat /var/version && echo ""
+set -euo pipefail
+
+# NOTE: The credhub cli does not ignore empty/null environment variables.
+# https://github.com/cloudfoundry-incubator/credhub-cli/issues/68
+if [ -z "${CREDHUB_CA_CERT}" ]; then
+  unset CREDHUB_CA_CERT
+fi
+
+credhub --version
+
+# shellcheck disable=SC2086
+
+flags=("")
+mkdir templates
+cat "${TEMPLATE}" > "templates/${FILENAME}"
+mkdir -p interpolated-files/credhub-get
+
+# ${flags[@] needs to be globbed to pass through properly
+# shellcheck disable=SC2068
+
+credhub interpolate \
+  --file templates/"${FILENAME}" ${flags[@]} \
+  >interpolated-files/"${FILENAME}"
+
+credhub import -f interpolated-files/"${FILENAME}"
+
+# code_snippet credhub-get-script end
